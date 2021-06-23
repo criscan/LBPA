73d185ba9f6ce702cfa3ac285dec754870af9edf
Cristian Canales <criscan@users.noreply.github.com>
2021-06-23 08:47:03 -0400

    Add files via upload

--- LBPA_graphics.r
+++ LBPA_graphics.r
@@ -1,10 +1,14 @@
 rm(list=ls(all=TRUE)) # erasure all objects
-setwd('C:/..............') # set current directory
-system('./LBPA')  # for model running
-source('C:/................/read.admb.R')
+setwd('G:/Mi unidad/Instituto_Nacional_de_Pesca/Camaron_Pomada/LBPA') # set current directory
+
+#system('./LBPA -ind lbpa4.dat')  # for model running
+#system('./LBPA -ind lbpa8.dat')  # for model running
+system('./LBPA -ind lbpa12.dat')  # for model running
+
+source('C:/Users/cristian/Desktop/LBPA-master/LBPA-master/read.admb.R')
 data <-read.rep('For_R.rep')
 
-nages<-10 #this depends on the specie
+nages<-6 #this depends on the specie
 
 nages <- length(data$Probability_of_length[,1])
 age <- seq(1,nages) 
@@ -72,9 +76,88 @@ plot(BinLen,S1, ylab="Frequency", xlab="Length", type="l", lwd=3,  xlim = c(min(
 lines(BinLen,S2, ylab="Frequency", xlab="Length", lwd=3, col="green", xlim = c(min(BinLen),1.1*max(BinLen)))
 legend(x = "topright",legend=c("Selectivity","Maturity"), bty = "n", col=c(1,3), lwd=3)
 
-tabla <- matrix(ncol=1, round(c(data$F_L50_slope_a0_cv_Lr_Ftar,Fcr_est/Ftar,SPR_est),2))
-rownames(tabla) <- c("F curr", "L50", "Slope", "a0", "cv", "Lr", "F Target","F curr/F Target","SPR")
-colnames(tabla) <- c("Value")
-outcomes<-as.table(tabla)
-outcomes
+#---------------------------------------------------------------------------------------------------------
+Nage<-data$Age_Length_s.e_N_Catch_Selectivity_F_Weight_Maturity[,4]
+Cage<-data$Age_Length_s.e_N_Catch_Selectivity_F_Weight_Maturity[,5]
+Fage<-data$Age_Length_s.e_N_Catch_Selectivity_F_Weight_Maturity[,7]
+Nage0<-data$Age_Length_s.e_N_Catch_Selectivity_F_Weight_Maturity[,4]
+
+M=-log(Nage[2]/Nage[1])-Fage[1]
+
+# CALCULO N0
+for (i in 2:nages) {
+  Nage0[i]=Nage0[i-1]*exp(-M)
+}
+Nage0[nages]=Nage0[nages]/(1-exp(-M)) 
+
+
+Cagelength<-ProbLen
+Nagelength<-ProbLen
+Nage0length<-ProbLen
+
+
+for (i in 1:nages) {
+  Cagelength[i,]<-Cage[i]*ProbLen[i,]/sum(Cage)
+  Nagelength[i,]<-Nage[i]*ProbLen[i,]/sum(Cage)
+  Nage0length[i,]<-Nage0[i]*ProbLen[i,]/sum(Cage)
+}
+
+par(mfrow = c(2,2))
+
+plot(BinLen,Nage0length[1,], 
+     ylab="Frequency", 
+     xlab="Length", 
+     type="l",cex.lab = 1.5, 
+     lwd=4, 
+     col="lightblue", 
+     xlim = c(min(BinLen),1.1*max(BinLen)),
+     ylim = c(0,max(Nage0length)),
+     main="Unfished Population at-length",cex.main = 1.5)
+
+
+for (i in 2:nages) {
+  lines(BinLen,Nage0length[i,], type="l", lwd=4, col="lightblue")
+}
+
+lines(BinLen,colSums(Nage0length), type="l", lwd=3, col="black")
+#-----------------------------------------------------------
+
+plot(BinLen,Nagelength[1,], 
+     ylab="Frequency", 
+     xlab="Length", 
+     type="l", cex.lab = 1.5,
+     lwd=4, 
+     col="lightblue", 
+     xlim = c(min(BinLen),1.1*max(BinLen)),
+     ylim = c(0,max(Nagelength)),
+     main="Current population at-length",cex.main = 1.5)
+
+
+for (i in 2:nages) {
+  lines(BinLen,Nagelength[i,], type="l", lwd=4, col="lightblue")
+}
+
+lines(BinLen,colSums(Nagelength), type="l", lwd=3, col="black")
+#--------------------------------------------------------
+
+plot(BinLen,Cagelength[1,], 
+     ylab="Frequency", 
+     xlab="Length", 
+     type="l", cex.lab = 1.5,
+     lwd=4, 
+     col="lightblue", 
+     xlim = c(min(BinLen),1.1*max(BinLen)),
+     ylim = c(0,max(Cagelength)),
+     main="Predicted Catch at-length",cex.main = 1.5)
+
+
+for (i in 2:nages) {
+  lines(BinLen,Cagelength[i,], type="l", lwd=4, col="lightblue")
+}
+lines(BinLen,PredFre, type="l", lwd=3, col="black")
+
+
+#--------------------------------------------------------
+
+
 
